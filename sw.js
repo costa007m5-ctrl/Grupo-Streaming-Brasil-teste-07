dff1bf3865d72677af50fffb5d7ae9e534e814c1
costa007m5-ctrl <costa007m5@gmail.com>
2025-10-22 21:53:18 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,4 @@
+// This file is no longer in use.
+// The Workbox caching logic has been merged into firebase-messaging-sw.js
+// to create a single, unified service worker.
+// This file can be safely deleted from your project.
